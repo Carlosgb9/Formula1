949a9d72d8c72d053b79fe7826a24b0989551cd6
Carlos Galera <Carlos Galera@DESKTOP-RBJJE0Q>
2023-12-07 09:27:59 +0100

    v

---
+++ Formula1/src/formula1/Pilot.java
@@ -0,0 +1,7 @@
+package formula1;
+
+public class Pilot {
+	private String name;
+	private int laps;
+	private int time;
+}
---
+++ Formula1/src/formula1/Race.java
@@ -0,0 +1,10 @@
+package formula1;
+
+public class Race {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
