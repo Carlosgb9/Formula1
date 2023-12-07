e8fb6eba7004ec7a7f6589ccafbd7e3dfc8d3f77
Carlos Galera <Carlos Galera@host.docker.internal>
2023-12-07 10:35:42 +0100

    hyh

--- Formula1/src/formula1/Pilot.java
+++ Formula1/src/formula1/Pilot.java
@@ -1,7 +1,50 @@
 package formula1;
 
-public class Pilot {
+public class Pilot implements Runnable {
 	private String name;
 	private int laps;
 	private int time;
+	private int sleepTime;
+	private RaceStatus rs;
+	
+	public Pilot(String name, int laps) {
+		super();
+		this.name = name;
+		this.laps = laps;
+		this.time = 0;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLaps() {
+		return laps;
+	}
+	public void setLaps(int laps) {
+		this.laps = laps;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	@Override
+	public void run() {
+		try {
+			sleepTime = (int) (Math.random() * 23) + 6;
+			Thread.sleep(sleepTime);
+			rs.lap(this);
+		}catch(InterruptedException e) {	
+		}
+	}
+	
+	
+	
+	
+	
 }
--- Formula1/src/formula1/RaceStatus.java
+++ Formula1/src/formula1/RaceStatus.java
@@ -1,5 +1,40 @@
 package formula1;
 
-public class RaceStatus {
+import java.util.ArrayList;
+import java.util.List;
 
+public class RaceStatus {
+	private static final int AMPLE_NOM = 18;
+	private static final int AMPLE_TEMPS =15;
+	private List<Pilot> score = new ArrayList<>();
+	private boolean finish = false;
+	
+	public synchronized void lap(Pilot pilot) {
+		System.out.println("Pilot: [" + pilot.getName() + ", laps=" + pilot.getLaps() + "]");
+		if (pilot.getLaps() == 0) {
+			finish = true;
+		}
+		if (isFinished()) {
+			score.add(pilot);
+		}
+	}
+	
+	public boolean isFinished() {
+		return finish;
+	}
+
+	@Override
+	public String toString() {
+		String result = "";
+		int pos = 0;
+		result += "[RACE RESULTS]";
+		result += System.lineSeparator();
+		for(Pilot p : score) {
+			result += pos + ". " +
+		}
+		return result;
+	}
+	
+	
+	
 }
